f5d6be7f5df7fd219d064f7c53be2ca863fd8299
tomasrpita <tomasrpita@gmail.com>
2025-01-03 14:06:17 +0100

    Add tailwind.config and ignores to .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './party/templates/party/**/*.html',
+    './party/static/party/js/**/*.js',
+    './party/static/party/src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
